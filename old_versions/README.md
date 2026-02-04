5593cf0e1ae8013f3907a78932064d3ccf1a0205
Pullusb <bernou.samuel@gmail.com>
2026-02-04 15:52:54 +0100

    replace slicing script with simplified version. Use async command instead of basic execute
    `mp.command_native_async`  instead of `os.execute`

---
+++ old_versions/README.md
@@ -0,0 +1,5 @@
+## old version
+
+This directory contains old versions of script, mainly for reference.
+
+Old script used `os.system` to call the command that seem to fail silently on some install.
--- slicing.lua
+++ slicing.lua
@@ -2,74 +2,20 @@ local msg = require "mp.msg"
 local utils = require "mp.utils"
 local options = require "mp.options"
 
--- v1.1
-
--- TODO make things more like this one : https://github.com/snylonue/mpv_slicing_copy/blob/master/slicing_copy.lua
--- e.g: use subprocess instead of execute
+-- v1.2 - Updated to use subprocess instead of os.execute
 
 local cut_pos = nil
 local copy_audio = 0
--- iterate on tree modes
+-- iterate on three modes
 -- 0 'normal' (video+audio), 1 'only_audio' (mp3), 2 'no_audio' (video)
-
-
--- original super RAW settings (montruously heavy weight video).
---[[ local o = {
-    target_dir = "~",
-    vcodec = "rawvideo",
-    acodec = "pcm_s16le",
-    prevf = "",
-    vf = "format=yuv444p16$hqvf,scale=in_color_matrix=$matrix,format=bgr24",
-    hqvf = "",
-    postvf = "",
-    opts = "",
-    ext = "avi",
-    command_template = [[
-        ffmpeg -v warning -y -stats
-        -ss $shift -i "$in" -t $duration
-        -c:v $vcodec -c:a $acodec $audio
-        -vf $prevf$vf$postvf $opts "$out.$ext"
-    #!--need two closing bracket here --,
-}]]
-
-
--- Copy original stream
---[[ local o = {
-    target_dir = "~",
-    vcodec = "copy",
-    acodec = "copy",
-    prevf = "",
-    vf = "", 
-    hqvf = "",
-    postvf = "",
-    opts = "",
-    ext = "mp4",--need to check extension
-    -- ext = "avi",
-    command_template = [[
-        ffmpeg -v warning -y -stats
-        -ss $shift -i "$in" -t $duration
-        -c:v $vcodec -c:a $acodec $audio
-        "$out.$ext"
-    #!--need two closing bracket here --,
-} ]]
 
 -- Reencode to mp4
 local o = {
     target_dir = "~",
-    vcodec = "",-- add custom reencode settings here and add the $vcodec in template (basic mp4 settings is pretty good already)
+    vcodec = "",-- add custom reencode settings here (basic mp4 settings is pretty good already)
     acodec = "",
-    prevf = "",
-    vf = "", 
-    hqvf = "",
-    postvf = "",
     opts = "",
     ext = "mp4",
-    -- ext = "avi",
-    command_template = [[
-        ffmpeg -v warning -y -stats
-        -ss $shift -i "$in" -t $duration
-        $audio "$out.$ext"
-    ]],
 }
 
 options.read_options(o)
@@ -79,7 +25,6 @@ function timestamp(duration)
     local minutes = duration % 3600 / 60
     local seconds = duration % 60
     return string.format("%02d_%02d_%02.03f", hours, minutes, seconds)
-    --return string.format("%02d:%02d:%02.03f", hours, minutes, seconds)
 end
 
 function timestamp_sec_round(duration)
@@ -87,7 +32,6 @@ function timestamp_sec_round(duration)
     local minutes = duration % 3600 / 60
     local seconds = duration % 60
     return string.format("%02d_%02d_%02.0f", hours, minutes, seconds)
-    --return string.format("%02d:%02d:%02.03f", hours, minutes, seconds)
 end
 
 function osd(str)
@@ -95,14 +39,11 @@ function osd(str)
 end
 
 function get_homedir()
-  -- It would be better to do platform detection instead of fallback but
-  -- it's not that easy in Lua.
-  return os.getenv("HOME") or os.getenv("USERPROFILE") or ""
+    return os.getenv("HOME") or os.getenv("USERPROFILE") or ""
 end
 
 function log(str)
     local logpath = utils.join_path(
-        -- o.target_dir:gsub("~", get_homedir()),
         mp.get_property("working-directory"),
         "mpv_slicing.log")
     f = io.open(logpath, "a")
@@ -112,72 +53,101 @@ function log(str)
     f:close()
 end
 
-function escape(str)
-    -- FIXME(Kagami): This escaping is NOT enough, see e.g.
-    -- https://stackoverflow.com/a/31413730
-    -- Consider using `utils.subprocess` instead.
-
-    -- return str:gsub("\\", "\\\\"):gsub('"', '\\"')
-    return str:gsub("\\", "/"):gsub('"', '\\"')
-end
-
-function trim(str)
-    return str:gsub("^%s+", ""):gsub("%s+$", "")
-end
-
-function get_csp()
-    local csp = mp.get_property("colormatrix")
-    if csp == "bt.601" then return "bt601"
-        elseif csp == "bt.709" then return "bt709"
-        elseif csp == "smpte-240m" then return "smpte240m"
-        else
-            local err = "Unknown colorspace: " .. csp
-            osd(err)
-            error(err)
-    end
-end
-
 function get_outname(shift, endpos)
     local name = mp.get_property("filename")
     local dotidx = name:reverse():find(".", 1, true)
     if dotidx then name = name:sub(1, -dotidx-1) end
     name = name:gsub(" ", "_")
     name = name:gsub(":", "-")
-    -- name = name .. string.format(".%s-%s", timestamp_sec_round(shift), timestamp_sec_round(endpos))-- separate time wth a dot
-    name = name .. string.format("-%s-%s", timestamp_sec_round(shift), timestamp_sec_round(endpos))-- separate time with a dash
+    name = name .. string.format("-%s-%s", timestamp_sec_round(shift), timestamp_sec_round(endpos))
     return name
 end
 
 function cut(shift, endpos)
-    local cmd = trim(o.command_template:gsub("%s+", " "))
-    local inpath = escape(utils.join_path(
+    local inpath = utils.join_path(
         utils.getcwd(),
-        mp.get_property("stream-path")))
-    local outpath = escape(utils.join_path(
-        -- o.target_dir:gsub("~", get_homedir()),
+        mp.get_property("stream-path"))
+    local outpath = utils.join_path(
         mp.get_property("working-directory"),
-        get_outname(shift, endpos)))
-
-    cmd = cmd:gsub("$shift", shift)
-    cmd = cmd:gsub("$duration", endpos - shift)
-    cmd = cmd:gsub("$vcodec", o.vcodec)
-    cmd = cmd:gsub("$acodec", o.acodec)
-    cmd = cmd:gsub("$audio", copy_audio < 2 and "" or "-an")
-    cmd = cmd:gsub("$prevf", o.prevf)
-    cmd = cmd:gsub("$vf", o.vf)
-    cmd = cmd:gsub("$hqvf", o.hqvf)
-    cmd = cmd:gsub("$postvf", o.postvf)
-    cmd = cmd:gsub("$matrix", get_csp())
-    cmd = cmd:gsub("$opts", o.opts)
+        get_outname(shift, endpos))
     
-    -- Beware that input/out filename may contain replacing patterns.
-    cmd = cmd:gsub("$ext", copy_audio == 1 and "mp3" or o.ext)
-    cmd = cmd:gsub("$out", outpath)
-    cmd = cmd:gsub("$in", inpath, 1)
-
-    msg.info(cmd)
-    log(cmd)
-    os.execute(cmd)
+    local ext = copy_audio == 1 and "mp3" or o.ext
+    local outfile = outpath .. "." .. ext
+    
+    local args = {
+        "ffmpeg",
+        "-v", "warning",
+        "-y",
+        "-stats",
+        "-ss", tostring(shift),
+        "-i", inpath,
+        "-t", tostring(endpos - shift),
+    }
+    
+    -- Add video codec if specified
+    if o.vcodec ~= "" then
+        table.insert(args, "-c:v")
+        table.insert(args, o.vcodec)
+    end
+    
+    -- Add audio codec if specified
+    if o.acodec ~= "" then
+        table.insert(args, "-c:a")
+        table.insert(args, o.acodec)
+    end
+    
+    -- Audio handling
+    if copy_audio == 1 then
+        -- Audio only mode (mp3)
+        table.insert(args, "-vn")
+    elseif copy_audio == 2 then
+        -- No audio mode
+        table.insert(args, "-an")
+    end
+    
+    -- Add extra options if specified
+    if o.opts ~= "" then
+        for opt in o.opts:gmatch("%S+") do
+            table.insert(args, opt)
+        end
+    end
+    
+    table.insert(args, outfile)
+    
+    local cmd_str = table.concat(args, " ")
+    msg.info(cmd_str)
+    log(cmd_str)
+    
+    osd("Encoding started...")
+    
+    mp.command_native_async({
+        name = "subprocess",
+        args = args,
+        playback_only = false,
+        capture_stdout = true,
+        capture_stderr = true,
+    }, function(success, result, error)
+        if success and result.status == 0 then
+            local out_name = get_outname(shift, endpos) .. "." .. ext
+            osd("Slice saved: " .. out_name)
+            msg.info("Slice completed successfully: " .. out_name)
+        else
+            local err_msg = "FFmpeg failed"
+            if result and result.status then
+                err_msg = err_msg .. " (code " .. result.status .. ")"
+            end
+            if error then
+                err_msg = err_msg .. ": " .. tostring(error)
+                msg.error("Error: " .. tostring(error))
+            end
+            if result and result.stderr and result.stderr ~= "" then
+                msg.error("stderr: " .. result.stderr)
+            end
+            osd(err_msg)
+            msg.error(err_msg)
+            log("ERROR: " .. err_msg)
+        end
+    end)
 end
 
 function toggle_mark()
@@ -206,7 +176,6 @@ function toggle_audio()
     if copy_audio == 0 then
         copy_audio = 1
         osd("Audio Only")
-        -- osd("Audio capturing : " .. (copy_audio and "enabled" or "disabled"))
     elseif copy_audio == 1 then
         copy_audio = 2
         osd("Audio Disabled")
@@ -214,8 +183,6 @@ function toggle_audio()
         copy_audio = 0
         osd("Audio Enabled")
     end
-
-        
 end
 
 mp.add_key_binding("c", "slicing_mark", toggle_mark)
